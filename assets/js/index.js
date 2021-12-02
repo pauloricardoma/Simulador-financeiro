7271c5745a5146ec0146d6cb60a0255c0bb37149
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2021-12-01 23:53:30 -0300

    0novas correçoes

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,9 @@ function simular() {
   const valortotal = document.getElementById('valor_total');
   const prazoanos = document.getElementById('prazo_anos');
   const jurosanual = document.getElementById('juros_anual');
-
+  const res_prazo_meses = document.getElementById('res_prazo_meses');
+  const res_juros_mes = document.getElementById('res_juros_mes');
+  const res_juros_acm = document.getElementById('res_juros_acm');
   // Cálculo dos juros mensais, do prazo em meses e da taxa de amortização paga/mês
   let jurosmensal = Math.pow(1 + jurosanual.valueAsNumber, 1 / 12) - 1;
   let prazomensal = prazoanos.valueAsNumber * 12;
@@ -16,7 +18,7 @@ function simular() {
 
   // Variável pra guardar o valor do cálculo dos juros por prestação e variável pra guardar o valor do total, juros por prestação mais taxa de amortização
   let jurosacmarray = [];
-  total = [];
+  let total = [];
 
   // Loop para cálculo do somatório de valores para o total dos juros acumulados, para o numero das prestações, sua amortização, juros por prestação e o total por prestação
   let jurosacm = 0;
